ade5d0ca161cb78139fd6c85585f63d4a63dbf7b
Matthieu Prat <matthieu.prat@dailymotion.com>
2015-11-24 15:43:22 +0100

    Complete level 1

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,16 @@
-require "json"
+require 'json'
+require 'date'
 
-# your code
+data = JSON.parse(File.read('data.json'), symbolize_names: true)
+
+rentals = data[:rentals].map do |rental|
+  car = data[:cars].find { |c| c[:id] == rental[:car_id] }
+  days = 1 + (Date.parse(rental[:end_date]) - Date.parse(rental[:start_date])).to_i
+  price = car[:price_per_day] * days + car[:price_per_km] * rental[:distance]
+
+  { id: rental[:id], price: price }
+end
+
+File.open('output.json', 'w') do |f|
+  f.puts(JSON.pretty_generate({ rentals: rentals }))
+end
